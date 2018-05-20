c7773340af04c8d2500058f5743ec30dfc14e7d4
pascales <pascales.dedy@gmail.com>
2018-05-20 14:23:37 +0700

    [Pascales] add serializer pagination

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'puma', '~> 3.11'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
+gem 'active_model_serializers'
+gem 'will_paginate'
+gem 'faker'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,10 +1,11 @@
+module V1
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   # GET /todos
   def index
-     # get current user todos
-    @todos = current_user.todos
+    # get paginated current user todos
+    @todos = current_user.todos.paginate(page: params[:page], per_page: 20)
     json_response(@todos)
   end
 
@@ -42,4 +43,5 @@ class TodosController < ApplicationController
   def set_todo
     @todo = Todo.find(params[:id])
   end
+end
 end
